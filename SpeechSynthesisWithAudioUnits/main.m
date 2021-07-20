bea1f7cbb5b4701294f819f0941ce55d4c70e8ad
Panagiotis Matsinopoulos <panagiotis@matsinopoulos.gr>
2021-07-20 15:36:54 +0300

    Now works with a Matrix Reverb audio unit in between

--- SpeechSynthesisWithAudioUnits/main.m
+++ SpeechSynthesisWithAudioUnits/main.m
@@ -32,6 +32,19 @@ void CreateMyAUGraph(MyAUGraphPlayer *player) {
              "Graph Add Audio Unit Speech Synthesis Node");
   // ---------------------------------------------------------
   
+  // Creating the Matrix Reverb Audio Unit graph Node
+  AudioComponentDescription effectDescription = {0};
+  effectDescription.componentType = kAudioUnitType_Effect;
+  effectDescription.componentSubType = kAudioUnitSubType_MatrixReverb;
+  effectDescription.componentManufacturer = kAudioUnitManufacturer_Apple;
+  
+  AUNode matrixReverbNode;
+  CheckError(AUGraphAddNode(player->graph,
+                            &effectDescription,
+                            &matrixReverbNode),
+             "Graph Add Audio Unit Matrix Reverb Node");
+  //----------------------------------------------------------
+  
   // Creating an Output Type Audio Unit with DefaultOuptut subtype
   AudioComponentDescription outputDefault = {0};
   outputDefault.componentType = kAudioUnitType_Output;
@@ -49,9 +62,16 @@ void CreateMyAUGraph(MyAUGraphPlayer *player) {
   CheckError(AUGraphConnectNodeInput(player->graph,
                                      speechSynthesisNode,
                                      0,
+                                     matrixReverbNode,
+                                     0),
+             "Connecting Speech Synthesis Node to Matrix Reverb Node");
+  
+  CheckError(AUGraphConnectNodeInput(player->graph,
+                                     matrixReverbNode,
+                                     0,
                                      outputDefaultNode,
                                      0),
-             "Connecting file player node to output node");
+             "Connecting Matrix Reverb Node to Output Default Node");
   
   // Open the Graph
   CheckError(AUGraphOpen(player->graph), "Opening Graph");
@@ -60,7 +80,7 @@ void CreateMyAUGraph(MyAUGraphPlayer *player) {
   CheckError(AUGraphInitialize(player->graph), "Initializing the Audio Unit Graph");
 }
 
-void PrepareSpeechAU(MyAUGraphPlayer *player) {
+void PrepareSpeechSynthesisAudioUnit(MyAUGraphPlayer *player) {
   AUNode speechSynthesisNode;
   CheckError(AUGraphGetIndNode(player->graph,
                                0,
@@ -94,12 +114,38 @@ void PrepareSpeechAU(MyAUGraphPlayer *player) {
                                   &dataSize),
              "Getting the chanel");
   
-  SpeakCFString(channel, CFSTR("How are you dear?"), NULL);
+  SpeakCFString(channel, CFSTR("Good Morning my friend! How are you today?"), NULL);
+}
+
+void PrepareMatrixReverbAudioUnit(MyAUGraphPlayer *player) {
+  AUNode matrixReverbNode;
+  CheckError(AUGraphGetIndNode(player->graph,
+                               1,
+                               &matrixReverbNode),
+             "Getting access to the matrix reverb node");
+  
+  AudioUnit matrixReverbAudioUnit;
+  CheckError(AUGraphNodeInfo(player->graph,
+                             matrixReverbNode,
+                             NULL,
+                             &matrixReverbAudioUnit),
+             "Getting the Audio Unit of the matrix reverb node");
+  
+  UInt32 roomType = kReverbRoomType_LargeHall;
+  
+  CheckError(AudioUnitSetProperty(matrixReverbAudioUnit,
+                                  kAudioUnitProperty_ReverbRoomType,
+                                  kAudioUnitScope_Global,
+                                  0,
+                                  &roomType,
+                                  sizeof(UInt32)),
+             "Setting the value for the reverb room type");
 }
 
 void PrepareAudioUnitGraph(MyAUGraphPlayer *player) {
   CreateMyAUGraph(player);
-  PrepareSpeechAU(player);
+  PrepareSpeechSynthesisAudioUnit(player);
+  PrepareMatrixReverbAudioUnit(player);
 }
 
 int main(int argc, const char * argv[]) {
